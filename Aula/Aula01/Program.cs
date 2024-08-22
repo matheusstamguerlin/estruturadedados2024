3e62fa89f59b6380f7d58a7b992ff8cf484b05fc
Matheus da Silva Stamguerlin <371475@unoesc.lan>
2024-08-21 21:45:37 -0300

    a aula de hoje

--- Aula/Aula01/Program.cs
+++ Aula/Aula01/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿    //comentario de uma linha
+    /*
+    Comentario de Multiplas Linhas
+    /*
+
+    //Declarando Variavel 
+*/
+using Aula01;
+
+int number;
+//atribuindo valor
+number = 10;
+
+//declarando constante
+const int DAYS_IN_WEEK = 7;
+
+//Imprimindo String Concatenada
+//Metodo tem nomes de verbos
+Console.WriteLine($"A Semana Tem {DAYS_IN_WEEK}dias");
+
+// new TipoEnumerador () -> Invoca o método construtor do objeto
+// Toda classe tem o seu construtor padrão implícito sem argumentos/params
+// É possível sobrescrever este método especificando argumentos
+// Todavia, se o fizermos, perdemos o original implicito e
+// precisaremos defini-lo explicitamente
+TipoEnumerador tipoEnum = new TipoEnumerador();
+
+TipoEnumerador.Language enumEnglish =
+      tipoEnum.GetLanguageEnum ("ingles");
+
+Console.WriteLine($"O enum de english é {enumEnglish}");
