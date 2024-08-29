d10d66d89e055b6dc7429f13cfa42f6688682cb5
Matheus da Silva Stamguerlin <371475@unoesc.lan>
2024-08-28 21:30:58 -0300

    AulaArray

---
+++ Aula/Aula02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using Aula02;
+
+Person person = new Person();
+Console.WriteLine(person.Name);
+
+Person person1 = new Person("Matheus Stamguerlin,",23);
+Console.WriteLine(person1.Name);
+
+Person person2 = new Person();
+person2.Name = "Willian Ferreira";
+person2.Age = 22;
+Console.WriteLine(person2.Name);
+
+Person person3 = new Person ()
+{
+    Name = "Kalil da Rosa",
+    Age = 20
+};
+Console.WriteLine(person3.Name);
+
+
+
+
