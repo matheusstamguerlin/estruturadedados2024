c538f19e87c0d3b2ea85c26d415f6e47bd24b4b2
Matheus da Silva Stamguerlin <371475@unoesc.lan>
2024-09-04 20:23:21 -0300

    MultiDimensionalArray

---
+++ Aula/Aula03atividade/Program.cs
@@ -0,0 +1,30 @@
+﻿
+// Definir o tamanho dos vetores
+const int tamanho = 100;
+
+// Criar os vetores para armazenas números pares e ímpares
+int[] pares = new int[tamanho];
+int[] ímpares = new int[tamanho];
+
+// Preencher os vetores com os primeiros 100 números pares e ímpares
+for (int i = 0; i < tamanho; i++)
+{
+    pares[i] = i;
+    ímpares[i] = i + 1;
+}
+
+// Imprimir os números pares
+Console.WriteLine("Números pares:");
+for (int i = 0; i < tamanho; i++)
+{
+    Console.WriteLine(pares[i] + " ");
+}
+Console.WriteLine();   
+
+// Imprimir os números ímpares
+Console.WriteLine("Números ímpares:");
+for (int i = 0; i < tamanho; i++)
+{
+    Console.WriteLine(ímpares[i] + "   ");
+}
+Console.WriteLine();
---
+++ Aula/aula04/MultiDimensionalArray/Program.cs
@@ -0,0 +1,60 @@
+﻿/*
+// Criando um vetor multidimensional
+// Com 5 linhas e 2 colunas
+
+
+int[,] numbers = new int[5,2];
+
+// Criando um vetor tridimensional
+int [,,] triNumbers = new int[5,4,3];
+
+// Podemos inicializar os valores do vetor
+// no momento de sua declaração
+int[,] iniNumbers = new int[,]
+{
+    {9,15,-9},
+    {19,25,-90},
+    {29,35,-91},
+    {39,45,-92},
+    {49,55,-93},
+};
+
+// Para acessar os valores contidos neste
+// tipo de variável int
+int valor = iniNumbers[0,1]; // Neste caso = 15
+
+Console.WriteLine( $"iniNumbers[0,1] == {valor}" );
+
+*/
+
+        int[,] iniNumbers = new int[,]
+        {
+            {9, 15, -9},
+            {19, 25, -90},
+            {29, 35, -91},
+            {39, 45, -92},
+            {49, 55, -93},
+        };
+
+        // Variáveis para armazenar o maior número e sua posição
+        int maiorNumero = iniNumbers[0, 0];
+        int linhaMaior = 0;
+        int colunaMaior = 0;
+
+        // Percorrendo a matriz
+        for (int i = 0; i < iniNumbers.GetLength(0); i++)
+        {
+            for (int j = 0; j < iniNumbers.GetLength(1); j++)
+            {
+                if (iniNumbers[i, j] > maiorNumero)
+                {
+                    maiorNumero = iniNumbers[i, j];
+                    linhaMaior = i;
+                    colunaMaior = j;
+                }
+            }
+        }
+
+        // Exibindo o maior número e sua posição
+        Console.WriteLine($"O maior número é {maiorNumero} na posição [{linhaMaior}, {colunaMaior}]");
+    
