bdd2a177e0ceadd6c3d8a972bed70486c788ead8
Matheus da Silva Stamguerlin <371475@unoesc.lan>
2024-09-04 21:50:24 -0300

    JaggedArray

---
+++ Aula/Aula 04 - JaggedArray/Program.cs
@@ -0,0 +1,11 @@
+﻿// JeggedArray é tambem conhecido como um vetor de vetores;
+
+int [][] numbers = new int[5][];
+
+// Nesse caso [][] indica que é um vetor de vetor, neste exemplo um vetor de 4 posições contendo tam. indefinido;
+
+numbers[0] = new int[] {1,2,3};
+numbers[1] = new int[] {4,3,2,1,0};
+numbers[2] = new int[] {1,2,0};
+numbers[3] = new int[] {57};
+numbers[4] = null!;
